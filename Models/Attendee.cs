735b286e542f8ec0ed3eefaa1213cfc32ccc311b
ThomasGrant08 <thomas.grant@moirae.co.uk>
2023-06-20 17:44:48 +0100

    Create Attendee.cs

---
+++ Models/Attendee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlanAhead.Models
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+        [ForeignKey("Event")]
+        public int EventId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public int UserId { get; set; }
+    }
+}
